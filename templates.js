7c7a4e68687cdc901e0a1e2f362aebd9b6695d62
Clay <recurse@kc-web-dev.(none)>
2013-04-24 01:31:35 -0500

    added advanced template compilation and caching

---
+++ templates.js
@@ -0,0 +1,34 @@
+/**
+ * This is the main template compiling caching module.
+ *
+ * We use handlebars for compilation, and export an array of precompiled 
+ * template functions ready for use
+ *
+ * These can be consumed by:
+ *
+ * var templates = require("./templates").templates;
+ * templates["my_fancy_page.html"]({"some_key":"a value"});
+ *
+ * all templates in the "views" path will be compiled. this is set in server.js
+ */
+var fs = require("fs");
+var hb = require("handlebars");
+var server = require("./server");
+var app = server.app;
+var templates = [];
+var template_path = app.set("views");
+var files = fs.readdirSync(template_path);
+
+console.log("Compiling template cache...");
+files.forEach(
+  function(file,index,arr)
+  {
+    if(file.indexOf("html",file.length - 4) == -1) return; //skip any files that don't end in .html
+    console.log("Compiling template: " + file);
+    var markup = fs.readFileSync(template_path + file).toString();
+    templates[file] = hb.compile(markup);
+  });
+exports.templates = templates;
+
+console.log("Completed compiling templates.");
+
