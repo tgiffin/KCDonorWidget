2aab78c6efd1870a6252b40b142ffd23aeb6d71f
Clay <recurse@kc-web-dev.(none)>
2013-05-31 14:31:35 -0500

    updated GA code for donor widget

--- static/script/app/donor_widget.js
+++ static/script/app/donor_widget.js
@@ -8,7 +8,7 @@
     var screen_logic;
 
     //init google analytics
-    var _gaq=[["_setAccount","UA-30533633-1"],['_setDomainName','klearchoice.com']];
+    var _gaq=[["_setAccount","UA-30533633-2"],['_setDomainName','klearchoice.com']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
       g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
         s.parentNode.insertBefore(g,s)}(document,"script"));
